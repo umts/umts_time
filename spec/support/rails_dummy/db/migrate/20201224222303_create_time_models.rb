a1c0a3a7646ae71d0682891f196c2d334375ede0
benmelz <ben.j.melz@gmail.com>
2021-01-13 15:22:01 -0500

    Adjust migration

--- spec/support/rails_dummy/db/migrate/20201224222303_create_time_models.rb
+++ spec/support/rails_dummy/db/migrate/20201224222303_create_time_models.rb
@@ -1,12 +1,9 @@
 class CreateTimeModels < ActiveRecord::Migration[5.2]
   def change
     create_table :time_models do |t|
-      t.atomic_time :start_time
-      t.atomic_time :end_time
+      t.integer :start_time
+      t.integer :end_time
       t.timestamps
-
-      puts "PRIMARY KEYS: #{t.primary_keys}"
-      puts "COLUMNS: #{t.columns}"
     end
   end
 end
