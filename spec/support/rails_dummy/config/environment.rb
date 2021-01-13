1c55d4f3bd9043cfc3149510e2192b626143b024
benmelz <ben.j.melz@gmail.com>
2021-01-13 16:11:21 -0500

    Adjust integration configurations

--- spec/support/rails_dummy/config/environment.rb
+++ spec/support/rails_dummy/config/environment.rb
@@ -1,7 +1,3 @@
-# Load AtomicTime
-libdir = File.expand_path File.join(File.dirname(__FILE__), '../../../..', 'lib')
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-require 'atomic_time'
 
 # Load the Rails application.
 require_relative 'application'
--- spec/support/rails_helper.rb
+++ spec/support/rails_helper.rb
@@ -5,3 +5,5 @@ ENV['RAILS_ROOT'] = File.expand_path File.join(File.dirname(__FILE__), 'rails_du
 
 require 'rails_dummy/config/environment'
 require 'rspec/rails'
+
+require_relative 'spec_helper'
